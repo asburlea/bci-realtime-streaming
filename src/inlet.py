6c1d38a7de9f08a0fb808f03442c412a978a022a
asburlea <andreea.sburlea@yahoo.com>
2026-06-24 14:58:28 +0200

    updated pylsl resolve by prop stream acquisition

--- src/inlet.py
+++ src/inlet.py
@@ -2,11 +2,15 @@
 LSL EEG inlet wrapper
 """
 
-from pylsl import StreamInlet, resolve_stream
+from pylsl import StreamInlet, resolve_byprop
 
 def create_inlet(stream_type="EEG"):
     print("Looking for EEG stream...")
-    streams = resolve_stream("type", stream_type)
+    streams = resolve_byprop(
+        "type",
+        "EEG",
+        timeout=5
+    )
     inlet = StreamInlet(streams[0])
     return inlet
 
